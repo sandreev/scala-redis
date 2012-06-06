9b24bb089d1640280571dee3fd8763fd9481ef96
Sergey Andreev <sergey@qwhispr.com>
2012-06-05 20:20:06 -0700

    refactored RedisClusterZKUpdater

--- src/main/scala/com/redis/cluster/RedisClusterZkUpdater.scala
+++ src/main/scala/com/redis/cluster/RedisClusterZkUpdater.scala
@@ -1,32 +1,38 @@
 package com.redis.cluster
 
-import config.ZookeeperConfigManager
+import config.{ZKStringSerializer, ZookeeperConfigManager}
 import io.Source
 import org.I0Itec.zkclient.ZkClient
 
-
 object RedisClusterZkUpdater {
   def main(args: Array[String]) {
-    if (args.size != 3) {
-      println("USAGE: mapping_file zookeeper_connect zookeeper_path")
-      System.exit(1)
+    val (mappingFile, zkConnect, redisNodesPath) = args.toList match {
+      case Nil =>
+        println("USAGE: mapping_file zookeeper_connect zookeeper_path")
+        throw new RuntimeException("Illegal number of arguments")
+      case m :: Nil => (m, "localhost:2181", "/rediscluster/nodes")
+      case m :: z :: Nil => (m, z, "/rediscluster/nodes")
+      case m :: z :: r :: _ => (m, z, r)
     }
 
-    val redisNodesPath = args(2)
-    val nodesMapping = Source.fromFile(args(0)).getLines()
+    val nodesMapping = Source.fromFile(mappingFile).getLines().toList.map(_.trim)
+    println("READ %s nodes from file %s".format(nodesMapping.size, mappingFile))
 
-    println("READ %s nodes from file %s".format(nodesMapping.size, args(0)))
+    val zkClient = new ZkClient(zkConnect, 6000, 6000, ZKStringSerializer)
 
-    val zkClient = new ZkClient(args(1))
-
-    val nodeData = nodesMapping.map(_.trim).mkString(ZookeeperConfigManager.NODES_SEPARATOR)
+    val nodeData = nodesMapping.mkString(ZookeeperConfigManager.NODES_SEPARATOR)
 
     if (!zkClient.exists(redisNodesPath)) {
+      zkClient.createPersistent("/rediscluster")
       zkClient.createPersistent(redisNodesPath, nodeData)
       println("Created redis nodes record in zookeeper and loaded it with data")
     } else {
       zkClient.writeData(redisNodesPath, nodeData)
       println("Loaded redis nodes mapping in zookeeper")
     }
+
+    println("Current data in zookeeper: " + zkClient.readData(redisNodesPath))
+
+    zkClient.close()
   }
 }
