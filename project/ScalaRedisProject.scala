874cd071763221b36cc0aa3753861241ad672aac
romezz <roman@jetlore.com>
2013-03-22 14:16:43 -0700

    incremented the version

--- project/ScalaRedisProject.scala
+++ project/ScalaRedisProject.scala
@@ -8,7 +8,7 @@ object ScalaRedisProject extends Build
 
   lazy val commonSettings: Seq[Setting[_]] = Seq(
     organization := "net.debasishg",
-    version := "2.7.10",
+    version := "2.7.11",
     scalaVersion := "2.9.1",
     scalacOptions ++= Seq("-deprecation", "-unchecked")
   )
